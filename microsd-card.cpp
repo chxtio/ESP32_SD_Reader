d5479dff83c66cdd5f4e39ad7905ff2263e02409
chxtio <33184844+chxtio@users.noreply.github.com>
2025-04-15 02:24:43 -0700

    Create read and write functions

--- microsd-card.cpp
+++ microsd-card.cpp
@@ -1,51 +1,67 @@
 #include <SD.h>
 #include <SPI.h>
+
 #define SD_CLK 41   // SPI Clock (SCK)
 #define SD_DATA0 36 // Data Output (MISO)
 #define SD_DATA1 37 // Data Input (MOSI)
 #define CS 5        // Chip Select
+
 // Create new SPI instance
 SPIClass spi = SPIClass(FSPI);
-void setup()
-{
+
+void writeToFile(const char *path, const char *message) {
+    File file = SD.open(path, FILE_WRITE);
+    if (!file) {
+        Serial.print("Failed to open file for writing: ");
+        Serial.println(path);
+        return;
+    }
+
+    if (file.print(message)) {
+        Serial.print("\nFile written to ");
+        Serial.println(path);
+    } else {
+        Serial.print("Write failed for ");
+        Serial.println(path);
+    }
+    file.close();
+}
+
+void readFromFile(const char *path) {
+    File file = SD.open(path);
+    if (!file) {
+        Serial.print("Failed to open file for reading: ");
+        Serial.println(path);
+        return;
+    }
+
+    Serial.print("\nReading from ");
+    Serial.println(path);
+    // Serial.println("File contents:");
+    while (file.available()) {
+        Serial.write(file.read());
+    }
+    Serial.println(); 
+    file.close();
+}
+
+void setup() {
     Serial.begin(115200);
+    
     // Initialize SPI communication protocol with custom pins
     spi.begin(SD_CLK, SD_DATA0, SD_DATA1);
+    
     // Initialize SD card
     uint32_t freq = 80000000;
-    if (!SD.begin(CS, spi, freq))
-    {
+    if (!SD.begin(CS, spi, freq)) {
         Serial.println("Failed to mount SD card");
         return;
     }
     Serial.println("SD card initialized");
 
-    // Write file
-    File file = SD.open("/test2.txt", FILE_WRITE);
-    
-    if(!file){
-      Serial.println("Failed to open file");
-      return;
-    }
-    const char * message = "Test message";
-    Serial.println();
-    if(file.print(message)){
-      Serial.println("File written");
-    } else {
-      Serial.println("Write failed");
-    }
-    file.close();
-
-    // Read a file
-    file = SD.open("/test.txt");
-    if (!file) {
-      Serial.println("Failed to open file");
-      return;
-    }
-    Serial.println("File contents: ");
-    while (file.available()){
-      Serial.write(file.read());
-    }
-    file.close();
+    readFromFile("/test.txt");
+    writeToFile("/test2.txt", "Test message");
+    readFromFile("/test2.txt");
 }
+
 void loop() {}
