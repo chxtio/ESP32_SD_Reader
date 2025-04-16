4d951c7738b92205056ded41aaebd4beb16a16fd
chxtio <33184844+chxtio@users.noreply.github.com>
2025-04-15 23:12:20 -0700

    Write to csv with simulated depth readings

--- microsd-card.cpp
+++ microsd-card.cpp
@@ -1,5 +1,6 @@
 #include <SD.h>
 #include <SPI.h>
+#include <math.h>
 
 #define SD_CLK 41   // SPI Clock (SCK)
 #define SD_DATA0 36 // Data Output (MISO)
@@ -8,6 +9,12 @@
 
 // Create new SPI instance
 SPIClass spi = SPIClass(FSPI);
+
+// Time and sensor reading variables
+float startTime;
+float currentTime;
+float lastPrintTime = 0;
+float depth;
 
 // List files and sizes
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
@@ -76,6 +83,42 @@ void writeToFile(fs::FS &fs, const char *path, const char *message) {
     file.close();
 }
 
+void writeToCSV(fs::FS &fs, const char *path, const char *csvRow) {
+    File file = fs.open(path, FILE_APPEND);  
+    if (!file) {
+        Serial.print("Failed to open CSV for writing: ");
+        Serial.println(path);
+        return;
+    }
+    if (file.println(csvRow)) {  // Add newline automatically
+        Serial.print("CSV row written to ");
+        Serial.println(path);
+    } else {
+        Serial.print("CSV write failed for ");
+        Serial.println(path);
+    }
+    file.close();
+}
+
+void writeToCSV(fs::FS &fs, const char *path, float time, float depth) {
+  File file = fs.open(path, FILE_APPEND);
+  // File file = SD.open(filename, FILE_WRITE);
+  if (file) {
+    file.print(time, 3);     // prints time with 3 decimal places
+    file.print(",");
+    file.println(depth, 2);  // prints depth with 2 decimal places
+    file.close();
+  } else {
+    Serial.println("Failed to open file");
+  }
+}
+
+// simlulate depth increasing 
+float getDepth() {
+  depth += 0.05; 
+  return depth;
+}
+
 
 void setup() {
     Serial.begin(115200);
@@ -91,22 +134,55 @@ void setup() {
     }
     Serial.println("SD card initialized");
 
-    listDir(SD, "/", 0);
+    // readFromFile(SD, "/test.txt");
+    // writeToFile(SD, "/test2.txt", "Test message");
+    // readFromFile(SD, "/test2.txt");
 
-    readFromFile(SD, "/test.txt");
-    writeToFile(SD, "/test2.txt", "Test message");
-    readFromFile(SD, "/test2.txt");
+    startTime = millis() / 1000.0;  // ms to seconds
+    writeToCSV(SD, "/data.csv", "Time,Depth");
+    // readFromFile(SD, "/data.csv"); // Confirm csv contents
 
     listDir(SD, "/", 0);
 
     // Listing directory: /
-    //   FILE: sketch.ino  SIZE: 2515
+    //   FILE: sketch.ino  SIZE: 4172
     //   FILE: diagram.json  SIZE: 840
     //   FILE: buoyancy-system.chip.json  SIZE: 389
     //   FILE: buoyancy-system.chip.c  SIZE: 10293
     //   FILE: test.txt  SIZE: 194
-    //   FILE: test2.txt  SIZE: 12
+    //   FILE: data.csv  SIZE: 12
 
 }
 
-void loop() {}
+void loop() {
+  // Update readings
+  currentTime = (millis() / 1000.0) - startTime;
+  depth = getDepth(); 
+
+  // Log values to SD Card
+  writeToCSV(SD, "/data.csv", currentTime, depth);
+
+  // Periodically display csv contents
+  if (currentTime - lastPrintTime >= 10.0) {
+    lastPrintTime = currentTime;
+    readFromFile(SD, "/data.csv"); 
+}
+  
+  delay(1000);
+
+}
+
+
+// Reading from /data.csv
+// Time,Depth
+// 0.025,0.05
+// 1.031,0.10
+// 2.042,0.15
+// 3.048,0.20
+// 4.059,0.25
+// 5.065,0.30
+// 6.076,0.35
+// 7.082,0.40
+// 8.093,0.45
+// 9.099,0.50
+// 10.110,0.55
